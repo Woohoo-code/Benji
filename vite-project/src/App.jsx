95aa1885ec7d56759800befdcbcb3b0e0259d9d5
Woohoo-code <benjamin.scott.king@icloud.com>
2025-07-28 13:30:45 -0400

    hi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,10 +3,24 @@ import { useState } from "react"
 
 function App() {
   function Hi(){
+
 setg(g+1)
+
+localStorage.setItem("num", g+1)
 }
-let [g,setg] = useState(0)
-return <div><button onClick={Hi} >Click me</button><p>Number is equal to {g}</p></div>
+function upd(event)
+{
+  setvalue(event.target.value)
+}
+function upds(event)
+{if(value == "adminB66") {
+setg(Number(event.target.value))
+}
+
+}
+let [value,setvalue] = useState("")
+let [g,setg] = useState(Number(localStorage.getItem("num")))
+return <div><p>Number is equal to {g}</p><button onClick={Hi} >Click me</button><input type="text" value={value} onChange={upd}></input><input type="number" value={g} onChange={upds}></input></div>
 
 }
 
