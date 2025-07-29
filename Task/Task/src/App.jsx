c3c67f6e83ca90a1fa45c5e57148ad55670d394d
Woohoo-code <benjamin.scott.king@icloud.com>
2025-07-29 13:12:00 -0400

    Commited

---
+++ Task/Task/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  function add(){
+  let listey = []
+  for(let i = 0;i<task.length;i+=1){
+    listey.push(task[i])
+  }
+  listey.push(input)
+  settask(listey)
+
+  }
+  function Remove(j){
+  let lister = []
+  for(let i = 0;i<task.length;i+=1){
+  if( i !== j){
+    lister.push(task[i])
+  }
+  }
+  settask(lister)
+  }
+  let v = []
+  let [task, settask] = useState(["Task 1","Task 2"])
+  let [input, setinput] = useState("Example Task")
+  for(let i = 0;i<task.length;i+=1){
+  v.push(<li key={i}>{task[i]}<button onClick={()=>Remove(i)}>Click to remove</button></li>)
+  }
+function text(event){
+setinput(event.target.value)
+}
+return (<div>
+  <button onClick={add}>Add Task</button>
+  <input type='text' onChange={text} value={input}></input>
+  <ol>Task List {v}</ol>
+  </div>);
+
+
+
+}
+
+export default App
